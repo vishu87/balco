778397f872afbf3c597d47f2d77f26f1f3edadef
unknown <vashi@C29P115.bhelhwr.co.in>
2015-07-23 08:55:26 +0530

    Payment city/center wise part 1

--- payments/month_pay.php
+++ payments/month_pay.php
@@ -1,3 +1,30 @@
+<?php
+require_once 'phplot/phplot.php';
+
+//Define the object
+$plot = new PHPlot();
+
+//Define some data
+$example_data = array(
+     array('a',3),
+     array('b',5),
+     array('c',7),
+     array('d',8),
+     array('e',2),
+     array('f',6),
+     array('g',7)
+);
+$plot->SetDataValues($example_data);
+
+//Turn off X axis ticks and labels because they get in the way:
+$plot->SetXTickLabelPos('none');
+$plot->SetXTickPos('none');
+
+//Draw it
+$plot->DrawGraph();
+
+
+?>
 <script type="text/javascript">
 checked = true;
       function checkedAll () {
@@ -29,179 +56,203 @@ return $str;
 
 ?> 
 <div class="top_m color1">
-						<a href="students.php?type=browse" style="text-decoration:underline">Payments</a> :
-						</div>
-						<?php
-						if($_SESSION["PRIV"] == 'admin'){
-							$query_add='';
-						} else {
-							$groups = array();
-							$query_pay = mysql_query("SELECT groups from members_priv where payments = 1 and user_id = $_SESSION[MEM_ID] ");
-							while ($row_pay = mysql_fetch_array($query_pay)) {
-								array_push($groups, $row_pay["groups"]);
-							}
-							$query_add = " and first_group IN (".implode(',', $groups).") ";
-						}
-						
-						
-						if($_GET["dos"] && $_GET["doe"])
-						{
-							$dos = strtotime($_GET["dos"]);
-							$doe = strtotime($_GET["doe"]);
-							$sql_case="SELECT payment_history.*,  name, father, father_mob, father_email, group_name as groupid, center.center_name as center, city.city_name as train_city from payment_history join students on payment_history.student_id = students.id join groups on students.first_group = groups.id join center on groups.center_id = center.id join city on center.city_id = city.id WHERE payment_history.dor >= '$dos' AND payment_history.dor<='$doe' ".$query_add." ORDER BY dor DESC";
-						$result_case=mysql_query($sql_case);
-							?>		
+		<a href="students.php?type=browse" style="text-decoration:underline">Payments</a> :
+</div>
+	<?php
+
+	$query_add = '';
+	if(isset($city_id)){
+
+		if(isset($center_id)){
+			if(isset($group_id)){
+				if(in_array($group_id, $all_groups_access)){
+					$query_add = "and first_group IN (".$group_id.")";
+				} else {
+					$query_add = "and first_group IN (-1)";
+				}
+			} else {
+				$query_add = "and first_group IN (".$priv[$city_id][$center_id].")";
+			}
+		} else {
+
+			$group_select = array();
+			foreach ($priv[$city_id] as $value) {
+				array_push($group_select, $value);
+			}
+			$query_add = "and first_group IN (".implode(',', $group_select).")";
+		}
+	} else {
+		$query_add = "and first_group IN (".$group_ids.")";
+	}
+	
+	
+	if($_GET["dos"] && $_GET["doe"])
+	{
+		$dos = strtotime($_GET["dos"]);
+		$doe = strtotime($_GET["doe"]);
+		$sql_case="SELECT payment_history.*,  name, father, father_mob, father_email, group_name as groupid, center.center_name as center, city.city_name as train_city from payment_history join students on payment_history.student_id = students.id join groups on students.first_group = groups.id join center on groups.center_id = center.id join city on center.city_id = city.id WHERE payment_history.dor >= '$dos' AND payment_history.dor<='$doe' ".$query_add." ORDER BY dor DESC";
+		$result_case=mysql_query($sql_case);
+		$array_sum = array();
+		$array_x = array();
+		ob_start();
+		?>		
 						
 						
 <div style="margin:10px;">
-							Payments
-						
-						<div class="gen_table">
-							<table cellspacing="0" cellpadding="0" width="100%" id="myTable" class="tablesorter">
-							<thead>
-								<tr >
-									<th>
-									SN
-									</th>
-									<th>
-									
-									</th>
-									<th>
-									Payment Date
-									</th>
-									<th>
-									Start
-									</th>
-									<th>
-									End
-									</th>
-									<th>Reg Fee</th>
-									<th>Sub Fee</th>
-									<th width="40">
-									Amount
-									</th>
-									<th >
-									Month Plan
-									</th>
-									<th >
-									Payment 
-									</th>
-									<th>
-									Name
-									</th>
-									<th width ="40">
-									City
-									</th>
-									<th width ="40">
-									Center
-									</th>
-									<th width ="40">
-									Group
-									</th>
-									
-									<th>
-									Father
-									</th>
-									
-									
-									
-								</tr>
-								</thead>
-								<tbody>
-								<?php
-									
-									$count =1;
-									while($row = mysql_fetch_array($result_case))
-									{
-									
-									$schol = 'schol1';
-										
-									//$age = duration($row["dob"]);
-									if($count%2 ==0)
-									{
-										echo '<tr class="color2">';
-									}
-									else
-									{
-										echo '<tr >';
-									}
-									echo '
-									
-									<td class="'.$schol.'">
-									'.$count.'
-									</td>
-									<td class="'.$schol.'">
-									<a href="fpdf/payment/payment_print.php?id='.$row["id"].'" ><img src="icons/pdf_icon.png"</a>
-									</td>
-									<td class="'.$schol.'">';
-									echo date("M j, Y", $row["dor"]);
-									echo '</td><td class="'.$schol.'">';
-									echo date("M j, Y", $row["dos"]);
-									echo '</td><td class="'.$schol.'">';
-									echo date("M j, Y", $row["doe"]);
-									
-									echo '</td>
-									<td class="'.$schol.'">'.$row["reg_fee"].'</td>
-									<td class="'.$schol.'">'.$row["sub_fee"].'</td>
-									<td class="'.$schol.'">
-									<span style="';
-									if($row["verified"] == 1) echo 'background:#0f0; padding:3px 10px;';
-									echo '">'.$row["amount"].'</span>
-									</td>
-									<td class="'.$schol.'">
-									'.$row["months"].'
-									</td>
-									<td class="'.$schol.'">
-									'.$row["p_remark"].'
-									</td>
-									<td class="'.$schol.'">
-									'.$row["name"].'
-									</td>
-									<td class="'.$schol.'">
-									'.$row["train_city"].'
-									</td>
-									<td class="'.$schol.'">
-									'.$row["center"].'
-									</td>
-									<td class="'.$schol.'">
-									'.$row["groupid"].'
-									</td>';
-									
-									
-									
-									echo '
-									<td class="'.$schol.'" >
-									'.$row["father"].'<br>'.$row["father_mob"].'
-									</td>';
-									
-									
-									
-									
-									echo'
-								</tr>';
-									$count++;
-									}
-								
-								?>
-								</tbody>
-							</table>
-							
-							<BR>Check all: <input type='checkbox' name='checkall' onclick='checkedAll();' checked="true">&nbsp;&nbsp;&nbsp;<a href="list_payment_xl.php?case=<?php echo base64_encode($sql_case);?>">Export to excel</a>
-    
-
-
-
-						</div>
-						</div>
-					<?php
+		Payments
+	
+	<div class="gen_table">
+		<table cellspacing="0" cellpadding="0" width="100%" id="myTable" class="tablesorter">
+		<thead>
+			<tr >
+				<th>
+				SN
+				</th>
+				<th>
+				
+				</th>
+				<th>
+				Payment Date
+				</th>
+				<th>
+				Start
+				</th>
+				<th>
+				End
+				</th>
+				<th>Reg Fee</th>
+				<th>Sub Fee</th>
+				<th width="40">
+				Amount
+				</th>
+				<th >
+				Month Plan
+				</th>
+				<th >
+				Payment 
+				</th>
+				<th>
+				Name
+				</th>
+				<th width ="40">
+				City
+				</th>
+				<th width ="40">
+				Center
+				</th>
+				<th width ="40">
+				Group
+				</th>
+				
+				<th>
+				Father
+				</th>
+				
+				
+				
+			</tr>
+			</thead>
+			<tbody>
+			<?php
+				
+				$count =1;
+				while($row = mysql_fetch_array($result_case))
+				{
+					$month = date("M",$row["dor"]);
+					$year = date("y",$row["dor"]);
+					$mon_year = $month.'-'.$year;
+					if(!in_array($mon_year, $array_x)){
+						array_push($array_x, $mon_year);
 					}
-					else
-					{
-					
-					echo "Please select from the left";
+
+					if(!isset($array_sum[$mon_year])){
+						$array_sum[$mon_year] = array();
 					}
+
+					array_push($array_sum[$mon_year], $row["amount"]);
+				
+				$schol = 'schol1';
 					
-					?>
-					
-					
-					
+				//$age = duration($row["dob"]);
+				if($count%2 ==0)
+				{
+					echo '<tr class="color2">';
+				}
+				else
+				{
+					echo '<tr >';
+				}
+				echo '
+				
+				<td class="'.$schol.'">
+				'.$count.'
+				</td>
+				<td class="'.$schol.'">
+				<a href="fpdf/payment/payment_print.php?id='.$row["id"].'" ><img src="icons/pdf_icon.png"</a>
+				</td>
+				<td class="'.$schol.'">';
+				echo date("M j, Y", $row["dor"]);
+				echo '</td><td class="'.$schol.'">';
+				echo date("M j, Y", $row["dos"]);
+				echo '</td><td class="'.$schol.'">';
+				echo date("M j, Y", $row["doe"]);
+				
+				echo '</td>
+				<td class="'.$schol.'">'.$row["reg_fee"].'</td>
+				<td class="'.$schol.'">'.$row["sub_fee"].'</td>
+				<td class="'.$schol.'">
+				<span style="';
+				if($row["verified"] == 1) echo 'background:#0f0; padding:3px 10px;';
+				echo '">'.$row["amount"].'</span>
+				</td>
+				<td class="'.$schol.'">
+				'.$row["months"].'
+				</td>
+				<td class="'.$schol.'">
+				'.$row["p_remark"].'
+				</td>
+				<td class="'.$schol.'">
+				'.$row["name"].'
+				</td>
+				<td class="'.$schol.'">
+				'.$row["train_city"].'
+				</td>
+				<td class="'.$schol.'">
+				'.$row["center"].'
+				</td>
+				<td class="'.$schol.'">
+				'.$row["groupid"].'
+				</td>';
+				
+				echo '
+				<td class="'.$schol.'" >
+				'.$row["father"].'<br>'.$row["father_mob"].'
+				</td>';
+				
+				echo'
+			</tr>';
+				$count++;
+				}
+			
+			?>
+			</tbody>
+		</table>
+		
+		<BR>Check all: <input type='checkbox' name='checkall' onclick='checkedAll();' checked="true">&nbsp;&nbsp;&nbsp;<a href="list_payment_xl.php?case=<?php echo base64_encode($sql_case);?>">Export to excel</a>
+	</div>
+	</div>
+<?php
+
+$output = ob_get_contents();
+ob_end_clean();
+echo $output;
+
+
+}
+else
+{
+
+echo "Please select from the left";
+}
+
+?>
--- payments/payment_left.php
+++ payments/payment_left.php
@@ -1,34 +1,126 @@
+<?php
+$cities = array();
+$centers = array();
+$groups = array();
+$priv = array();
+$group_ids = array();
+$group_ids_edit = array();
 
+if($_SESSION['PRIV'] != 'admin'){
+	$sql_priv=mysql_query("SELECT members_priv.center_id, members_priv.groups, city.city_name, city.id as city_id, center.center_name from members_priv inner join center on members_priv.center_id = center.id inner join city on center.city_id = city.id WHERE members_priv.user_id='$_SESSION[MEM_ID]' and members_priv.payments = 1");
+	while ($row_priv = mysql_fetch_array($sql_priv)) {
+		$priv[$row_priv["city_id"]][$row_priv["center_id"]] = $row_priv["groups"];
+		$cities[$row_priv["city_id"]] = $row_priv["city_name"];
+		$centers[$row_priv["center_id"]] = $row_priv["center_name"];
+		array_push($group_ids, $row_priv["groups"]);
+	}
+
+	$sql_priv=mysql_query("SELECT  members_priv.groups from members_priv WHERE members_priv.user_id='$_SESSION[MEM_ID]' and members_priv.sp_edit = 1");
+	while ($row_priv = mysql_fetch_array($sql_priv)) {
+		array_push($group_ids_edit, $row_priv["groups"]);
+	}
+} else {
+	
+	$sql_priv=mysql_query("SELECT center.id as center_id, city.city_name, city.id as city_id, center.center_name from center inner join city on center.city_id = city.id ");
+	while ($row_priv = mysql_fetch_array($sql_priv)) {
+		
+		$gpx = array();
+		$sql_groups = mysql_query("SELECT id from groups where center_id = '$row_priv[center_id]' ");
+		while ($row_groups = mysql_fetch_array($sql_groups)) {
+
+			array_push($gpx, $row_groups["id"]);
+		}
+		$priv[$row_priv["city_id"]][$row_priv["center_id"]] = implode(',', $gpx);
+		$cities[$row_priv["city_id"]] = $row_priv["city_name"];
+		$centers[$row_priv["center_id"]] = $row_priv["center_name"];
+		array_push($group_ids, implode(',', $gpx));
+	}
+	$group_ids_edit = $group_ids;
+}
+$group_ids = implode(',', $group_ids);
+$all_groups_access = explode(',', $group_ids);
+
+if(sizeof($group_ids_edit) > 0){
+	$group_ids_edit = implode(',', $group_ids_edit);
+	$all_groups_access_edit = explode(',', $group_ids_edit);
+} else {
+	$all_groups_access_edit = array();
+}
+
+
+
+$group_names = mysql_query("SELECT id, group_name from groups where id IN (".$group_ids.") ");
+while ($row_group = mysql_fetch_array($group_names)) {
+	$groups[$row_group["id"]] = $row_group["group_name"];
+}
+
+$space_str1 = '&nbsp;&nbsp;&nbsp;<img src="bullet1.jpg" />&nbsp;';
+$space_str2 = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="bullet2.png" />&nbsp;';
+$space_str3 = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="bullet3.png" />&nbsp;';
+
+if(isset($_GET["city_id"])) $city_id = $_GET["city_id"];							
+if(isset($_GET["center_id"])) $center_id = $_GET["center_id"];							
+if(isset($_GET["group_id"])) $group_id = $_GET["group_id"];
+
+
+?>
 <table width="100%" cellspacing="0" cellpadding="0">
-						<tr class="color2">
-							<td >
-							<form action="?" method="get"><table cellspacing="0" cellpadding="0" width="100%">
-									<tr>
-											<td align="right" width="20%">Start</td>
-											<td><input type="text" size="12" name="dos" id="inputField" value="<?php echo $_GET["dos"];?>" />
-							</td>
-									</tr>
-									<tr>
-											<td align="right">End</td>
-											<td><input type="text" size="12" name="doe" id="inputField1" value="<?php echo $_GET["doe"];?>" /></td>
-									</tr>
-									
-								</table>
-								<div align="center">
-						<input type="SUBMIT" Value="GO" class="color3" style="border:0px; margin:10px;">
-						</div>
-							</form>
-							</td>
-						</tr>
+		<tr class="color2">
+		<td>
+			<a href="payments.php?type=verify">Payment Verifications</a>
+		</td>
+	</tr>
+	<tr>
+		<td>Payment Details</td>
+	</tr>
+	<tr class="color2">
+		<td >
+		<form action="?" method="get"><table cellspacing="0" cellpadding="0" width="100%">
+				<tr>
+						<td align="right" width="20%">Start</td>
+						<td><input type="text" size="12" name="dos" id="inputField" value="<?php echo $_GET["dos"];?>" />
+		</td>
+				</tr>
+				<tr>
+						<td align="right">End</td>
+						<td><input type="text" size="12" name="doe" id="inputField1" value="<?php echo $_GET["doe"];?>" /></td>
+				</tr>
+				
+			</table>
+			<div align="center">
+	<input type="SUBMIT" Value="GO" class="color3" style="border:0px; margin:10px; padding: 5px 20px">
+	</div>
+		</form>
+		</td>
+	</tr>
+	<tr>
+		<td><?php echo '<a href="?dos='.$_GET["dos"].'&amp;doe='.$_GET["doe"].'">View All</a>'; ?></td>
+	</tr>
 
-						<tr>
-							<td>
-								<a href="payments.php?type=verify">Payment Verifications</a>
-							</td>
-						</tr>
-						
-						
-						
-						
-						
-						</table>
+	<?php
+	foreach ($cities as $key => $city) {
+		echo '<tr ';
+		echo ($city_id == $key && $_GET["type"] == $type)?'class="color_c"':'';
+		echo '>';
+		echo '<td>'.$space_str1.'<a href="?dos='.$_GET["dos"].'&amp;doe='.$_GET["doe"].'&amp;city_id='.$key.'">'.$city.'</a></td>';
+		if($city_id == $key && $_GET["type"] == $type){
+			foreach ($priv[$key] as $center => $group) {
+				echo '<tr ';
+				echo ($center_id == $center)?'class="color_ce"':'';
+				echo '>';
+				echo '<td>'.$space_str2.'<a href="?dos='.$_GET["dos"].'&amp;doe='.$_GET["doe"].'&amp;city_id='.$key.'&amp;center_id='.$center.'">'.$centers[$center].'</a></td></tr>';
+				if($center_id == $center){
+					$center_groups = explode(',', $group);
+					foreach ($center_groups as $value) {
+						echo '<tr ';
+						echo ($group_id == $value)?'class="color_g"':'';
+						echo '>';
+						echo '<td>'.$space_str3.'<a href="?dos='.$_GET["dos"].'&amp;doe='.$_GET["doe"].'&amp;city_id='.$key.'&amp;center_id='.$center.'&amp;group_id='.$value.'">'.$groups[$value].'</a></td></tr>';
+					}
+				}
+			}
+		}
+	}
+	?>
+	</table>
+
